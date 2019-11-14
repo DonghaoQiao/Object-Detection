5c8954dddff0312dd76fb4f63bdd531b737222f2
Donghao(Jeremy) Qiao <qiaodonghao@gmail.com>
2019-11-14 12:34:01 -0500

    Update ReadImage.py

--- ReadImage.py
+++ ReadImage.py
@@ -1,26 +1,35 @@
-# Three methods to read grayscale image
 import matplotlib.pyplot as plt
-plt.figure(figsize=(15,5))
+
+# 4 ways to read image
+plt.figure(figsize=(10,10))
 
 # scikit-image
-plt.subplot(131)
+plt.subplot(221)
 from skimage import io
-img=io.imread('lena.jpg',as_gray=True)
+img = io.imread('lena.jpg')
 plt.title('skimage',fontsize=20)
 plt.imshow(img)
 
 # opencv
-plt.subplot(132)
+plt.subplot(222)
 import cv2
-img=cv2.imread('lena.jpg',cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('lena.jpg')
+img1 = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 plt.title('opencv',fontsize=20)
-plt.imshow(img)
+plt.imshow(img1)
 
 # pillow
-plt.subplot(133)
+plt.subplot(223)
 from PIL import Image
-img = Image.open('lena.jpg').convert('L')
+img = Image.open('lena.jpg')
 plt.title('PIL',fontsize=20)
+plt.imshow(img)
+
+# matplotlib
+plt.subplot(224)
+import matplotlib.image as mpimg
+img = mpimg.imread('lena.jpg')
+plt.title('matplotlib',fontsize=20)
 plt.imshow(img)
 
 plt.show()

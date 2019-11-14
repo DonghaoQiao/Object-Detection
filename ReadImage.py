65ccb26ce9e94dcb949918cb26241961c55030da
Donghao(Jeremy) Qiao <qiaodonghao@gmail.com>
2019-11-14 12:55:40 -0500

    Update ReadImage.py

--- ReadImage.py
+++ ReadImage.py
@@ -1,35 +1,70 @@
+# 4 ways to read image and grayscale image
 import matplotlib.pyplot as plt
 
-# 4 ways to read image
-plt.figure(figsize=(10,10))
+##### RGB image #####
+plt.figure(figsize=(6,6))
 
 # scikit-image
 plt.subplot(221)
 from skimage import io
 img = io.imread('lena.jpg')
-plt.title('skimage',fontsize=20)
+plt.title('skimage',fontsize=10)
 plt.imshow(img)
 
 # opencv
 plt.subplot(222)
 import cv2
-img = cv2.imread('lena.jpg')
-img1 = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-plt.title('opencv',fontsize=20)
-plt.imshow(img1)
+bgr = cv2.imread('lena.jpg')
+img = cv2.cvtColor(bgr,cv2.COLOR_BGR2RGB)
+plt.title('opencv',fontsize=10)
+plt.imshow(img)
 
 # pillow
 plt.subplot(223)
 from PIL import Image
 img = Image.open('lena.jpg')
-plt.title('PIL',fontsize=20)
+plt.title('PIL',fontsize=10)
 plt.imshow(img)
 
 # matplotlib
 plt.subplot(224)
 import matplotlib.image as mpimg
 img = mpimg.imread('lena.jpg')
-plt.title('matplotlib',fontsize=20)
+plt.title('matplotlib',fontsize=10)
+plt.imshow(img,cmap = plt.get_cmap('gray'))
+
+
+##### grayscale image #####
+plt.figure(figsize=(6,6))
+
+# scikit-image
+plt.subplot(221)
+from skimage import io
+img = io.imread('lena.jpg',as_gray=True)
+plt.title('skimage',fontsize=10)
+plt.imshow(img)
+
+# opencv
+plt.subplot(222)
+import cv2
+img = cv2.imread('lena.jpg',cv2.IMREAD_GRAYSCALE)
+plt.title('opencv',fontsize=10)
+plt.imshow(img)
+
+# pillow
+plt.subplot(223)
+from PIL import Image
+img = Image.open('lena.jpg').convert('L')
+plt.title('PIL',fontsize=10)
+plt.imshow(img)
+
+# matplotlib
+plt.subplot(224)
+import matplotlib.image as mpimg
+rgb = mpimg.imread('lena.jpg')
+r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+img = 0.2989 * r + 0.5870 * g + 0.1140 * b
+plt.title('matplotlib',fontsize=10)
 plt.imshow(img)
 
 plt.show()
